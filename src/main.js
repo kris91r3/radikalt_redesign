e6d8aa88d62d9986f7043ef95a4ab6757f508241
Kristina Andersen <kris91r3@stud.kea.dk>
2022-10-07 12:35:23 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+import Alpine from "alpinejs";
+window.Alpine = Alpine;
+Alpine.start();
+
+/* Import af Motion One bibliotek */
+import { animate, stagger, inView, scroll, timeline } from "motion";

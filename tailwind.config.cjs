816f13dce1a4c4c30c83b6d3109ef7863c130e36
Alberte <albe304d@stud.kea.dk>
2022-10-07 14:28:18 +0200

    forside halvvejs færdig

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rod: "#CF1919",
+        gul: "#F4F465",
+        sort: "#36322d",
+        bla: "#008180",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };

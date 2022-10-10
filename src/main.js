780dbb0f438966ba5936719e3ae9427aee925404
Kristina Andersen <kris91r3@stud.kea.dk>
2022-10-10 10:15:17 +0200

    forsøg

--- src/main.js
+++ src/main.js
@@ -5,3 +5,9 @@ Alpine.start();
 
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
+
+const btn = document.querySelector(".toggle-btn");
+
+const nav = document.querySelector("nav");
+
+// Burgermenu

eea6f2ad1e25c70068881a47a6435522f3526ff8
Kristina Andersen <kris91r3@stud.kea.dk>
2022-10-11 09:29:18 +0200

    vite.config html-sider

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,10 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         koebbillet: resolve(__dirname, "src/koebbillet.html"),
+        forestillinger: resolve(__dirname, "src/forestillinger.html"),
+        spilletider: resolve(__dirname, "src/spilletider.html"),
+        vaermed: resolve(__dirname, "src/vaermed.html"),
+
         // about: resolve(__dirname, "src/about.html"),
       },
     },

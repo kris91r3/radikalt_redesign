00e0b64a349dcdc5e6d007448015b414ba3b3488
Kristina Andersen <kris91r3@stud.kea.dk>
2022-10-14 12:21:29 +0200

    flere rettelser

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,18 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        rod: "#CF1919",
-        mellemrod: "#a80909",
         gul: "#F4F465",
         mellemgul: "#d6d63e",
         moerkgul: "#565606",
         sort: "#36322d",
-        bla: "#008180",
-        blabla: "#006969",
-        lysbla: "#E5F2F2",
+
         offwhite: "#e6e5e5",
         pink: "#e500b3",
         lilla: "#460137",
+        moerklilla: "#2c0022",
         blaa: "#19007b",
         lysgul: "#ffffe0",
         lysblaa: "#f8faff",
